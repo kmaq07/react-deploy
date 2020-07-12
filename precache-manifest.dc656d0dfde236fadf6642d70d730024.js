a15bb76da20a86ed69a293cee49060fbf3afe52d
kmaq07 <kqusalh484@gmail.com>
2020-07-12 18:12:39 +0400

    Updates

---
+++ precache-manifest.dc656d0dfde236fadf6642d70d730024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f75a4a6752201565671646985ae1867",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "7a73aac3c2d63ec0191f",
+    "url": "/react-deploy/static/css/2.3ea52fea.chunk.css"
+  },
+  {
+    "revision": "7a73aac3c2d63ec0191f",
+    "url": "/react-deploy/static/js/2.f3c5f579.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deploy/static/js/2.f3c5f579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0862332895e6d1063f9e",
+    "url": "/react-deploy/static/js/main.64e8c319.chunk.js"
+  },
+  {
+    "revision": "d06f247cc992b632f30c",
+    "url": "/react-deploy/static/js/runtime-main.c09fbe77.js"
+  }
+]);
