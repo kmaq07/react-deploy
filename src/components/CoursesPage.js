e6e22ced86752631b8c5815a6ba9368bec5e3e00
kmaq07 <kqusalh484@gmail.com>
2020-07-12 18:02:21 +0400

    First commit.

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import courseStore from "../stores/courseStore";
+import CourseList from "./CourseList";
+import { Link } from "react-router-dom";
+import { loadCourses, deleteCourse } from "../actions/courseActions";
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    courseStore.addChangeListener(onchange);
+    if (courseStore.getCourses().length === 0) loadCourses();
+    return () => courseStore.removeChangeListener(onchange); //cleanup on unmount
+  }, []);
+
+  function onchange() {
+    setCourses(courseStore.getCourses());
+  }
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add Course
+      </Link>
+      <CourseList courses={courses} deleteCourse={deleteCourse} />
+    </>
+  );
+}
+
+export default CoursesPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  const activeStyle = { color: "black" };
+
+  return (
+    <nav>
+      <NavLink activeStyle={activeStyle} exact to="/">
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={activeStyle} to="/courses">
+        Courses
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={activeStyle} to="/about">
+        About
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Header;
